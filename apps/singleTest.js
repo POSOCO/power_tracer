e1f507174f053ea7e93868b3af7b726f7b3c3f61
nagasudhirpulla <nagasudhirpulla@gmail.com>
2016-08-31 23:09:38 +0530

    added slider to change line flow interactively

--- apps/singleTest.js
+++ apps/singleTest.js
@@ -10,6 +10,8 @@ document.onreadystatechange = function () {
     }
 };
 
+var line2;
+var tracer;
 function onDomComplete() {
     var lineCanvas = document.getElementById("myCanvas");
     var line = new PowerLine({
@@ -22,14 +24,20 @@ function onDomComplete() {
         "power": 50,
         "levels": [0, 600, 800]
     });//(20,20);(110,110)
-    var line2 = new PowerLine({
+    line2 = new PowerLine({
         "ends": [[300, 250], [200, 450]],
         "power": 900,
         "levels": [0, 600, 800]
     });//(20,20);(110,110)
-    var tracer = new LineTracer({
+    tracer = new LineTracer({
         "canvas": lineCanvas,
         "lines": [line, line1, line2]
     });
+    tracer.plot_lines();
+}
+
+function showValue(newVal) {
+    document.getElementById("power_input_label").innerHTML = newVal;
+    line2.set_line_power(newVal);
     tracer.plot_lines();
 }
