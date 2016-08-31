7e5a0d45064bfa081fe22c51f3f191255197b1df
WRLDC <wrldcposoco@gmail.com>
2016-08-31 07:02:43 +0530

    created test logic file

---
+++ apps/singleTest.js
@@ -0,0 +1,12 @@
+"use strict";
+
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive") {
+        //onDomReady();
+    } else if (document.readyState == "complete") {
+        //DOM load initialization function
+        
+    }
+};
+
+var lineCanvas = document.getElementById("myCanvas");
